e67a388a066a4b1658857ceddb7d567fb4bc0eae
Raditya <arya.raditya26@gmail.com>
2017-04-02 02:34:16 +0700

    angular init & basic html

---
+++ js/controller/index.js
@@ -0,0 +1,9 @@
+app.controller('MainController', function($scope) {
+    $scope.dataList = [
+      {name:'tes one'},
+      {name:'tes two'},
+      {name:'tes three'},
+      {name:'tes four'},
+      {name:'tes five'},
+    ]
+})
