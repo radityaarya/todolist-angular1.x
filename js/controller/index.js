365945504535ff9df5c1c325fa675b63c8dc37bd
Raditya <arya.raditya26@gmail.com>
2017-04-02 23:02:09 +0700

    feature added : delete to do list

--- js/controller/index.js
+++ js/controller/index.js
@@ -10,11 +10,22 @@ app.controller('MainController', function($scope) {
 
             existingData === null ? existingData = [] : JSON.parse(localStorage.getItem("todolist"))
 
-            existingData.push({subject: $scope.addForm})
+            existingData.push({index: existingData.length + 1 , subject: $scope.addForm})
             localStorage.setItem("todolist", JSON.stringify(existingData))
 
             $scope.dataList = JSON.parse(localStorage.getItem("todolist"))
             $scope.addForm = ''
         }
     }
+
+    // DELETE data
+    $scope.remove=function(item){
+        var existingData = JSON.parse(localStorage.getItem("todolist"))
+        var index = $scope.dataList.indexOf(item)
+
+        existingData.splice(index,1);
+        localStorage.setItem("todolist", JSON.stringify(existingData))
+
+        $scope.dataList = JSON.parse(localStorage.getItem("todolist"))
+   }
 })
